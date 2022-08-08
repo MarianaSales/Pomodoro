6e424464aef422aea8ff60abb84f326671a13bd3
MarianaSales <38512371+MarianaSales@users.noreply.github.com>
2022-08-08 16:27:45 -0300

    Update reducer.ts

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -10,12 +10,13 @@ interface CycleState {
 
 export function cyclesReducers(state: CycleState, action: any) {
     switch (action.type) {
-        case ActionsTypes.ADD_NEW_CYCLE:
-            return produce(state, draft => {
+        case ActionsTypes.ADD_NEW_CYCLE: {
+            return produce(state, (draft) => {
                 draft.cycles.push(action.payload.newCycle);
                 draft.activeCycleId = action.payload.newCycle.id
             })
-        case ActionsTypes.INTERRUPT_CURRENT_CYCLE:
+        }
+        case ActionsTypes.INTERRUPT_CURRENT_CYCLE: {
             const currentCycleIndex = state.cycles.findIndex(cycle => {
                 return cycle.id === state.activeCycleId
             })
@@ -26,17 +27,19 @@ export function cyclesReducers(state: CycleState, action: any) {
                 draft.activeCycleId = null,
                     draft.cycles[currentCycleIndex].interrupteDate = new Date()
             })
-        case ActionsTypes.MARK_CURRENT_CYCLE_AS_FINESHED:
-            const currCycleIndex = state.cycles.findIndex(cycle => {
+        }
+        case ActionsTypes.MARK_CURRENT_CYCLE_AS_FINESHED: {
+            const currentCycleIndex = state.cycles.findIndex(cycle => {
                 return cycle.id === state.activeCycleId
             })
-            if (currCycleIndex < 0) {
+            if (currentCycleIndex < 0) {
                 return state
             }
             return produce(state, (draft) => {
                 draft.activeCycleId = null,
-                    draft.cycles[currCycleIndex].finishedDate = new Date()
+                    draft.cycles[currentCycleIndex].finishedDate = new Date()
             })
+        }
         default:
             return state
     }
